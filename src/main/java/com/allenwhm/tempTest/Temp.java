5378d0da2f7debc54fe34b4c1806c32c33bd10e9
allenwhm <wanghmjlu@163.com>
2018-07-07 22:24:44 +0800

    add temp test placeholder

---
+++ src/main/java/com/allenwhm/tempTest/Temp.java
@@ -0,0 +1,13 @@
+package com.allenwhm.tempTest;
+
+/**
+ * @author allenwhm
+ * @date 2018/7/7 22:23
+ * @description
+ */
+public class Temp {
+
+    public static void main(String[] args) {
+        System.out.println("this class is for temp test");
+    }
+}
