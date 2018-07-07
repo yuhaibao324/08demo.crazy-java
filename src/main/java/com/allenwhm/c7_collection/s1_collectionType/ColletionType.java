2e75aa68e4906a85efa5ec2cb8df2ef598791ba1
allenwhm <wanghmjlu@163.com>
2018-07-07 22:22:22 +0800

    c7_s1 collection type

---
+++ src/main/java/com/allenwhm/c7_collection/s1_collectionType/ColletionType.java
@@ -0,0 +1,25 @@
+package com.allenwhm.c7_collection.s1_collectionType;
+
+import java.util.*;
+
+/**
+ * @author allenwhm
+ * @date 2018/7/7 22:15
+ * @description
+ */
+public class ColletionType {
+
+    public static void main(String[] args) {
+
+        // the following is wrong, they are abstract method
+        /*Collection c1 = new List();
+        Collection c2 = new Set();
+        Collection c3 = new Queue();*/
+
+        List l1 = new ArrayList();
+        List l2 = new LinkedList();
+        List l3 = new Vector();
+
+
+    }
+}
